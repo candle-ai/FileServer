3dc74ea94b11183666df5ad2f66d0c17916f0ea6
victor <victor@trycandle.ai>
2026-01-27 19:16:54 +0100

    fix: rebuild Outlook add-in for dev environment with updated dependencies

--- dist-outlook-addin/assets/modulepreload-polyfill.DaKOjhqt.js
+++ dist-outlook-addin/assets/modulepreload-polyfill.DaKOjhqt.js
@@ -35,3 +35,4 @@
     fetch(link.href, fetchOpts);
   }
 })();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9kdWxlcHJlbG9hZC1wb2x5ZmlsbC5EYUtPamhxdC5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7Ozs7In0=
